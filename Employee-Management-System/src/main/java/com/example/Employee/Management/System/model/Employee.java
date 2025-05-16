6ac593ade96af8d3d30de199362e45ce22a417c4
VenuGopalReddy007 <venugopalreddy7482@gmail.com>
2025-05-16 16:10:45 +0530

    Update Employee.java

--- Employee-Management-System/src/main/java/com/example/Employee/Management/System/model/Employee.java
+++ Employee-Management-System/src/main/java/com/example/Employee/Management/System/model/Employee.java
@@ -33,4 +33,86 @@ public class Employee {
     private boolean active;
 
 
+    //if you are facing a probem like lombok not working you can use the below getters and setters
+    //     public Long getId() {
+    //     return id;
+    // }
+
+    // public void setId(Long id) {
+    //     this.id = id;
+    // }
+
+    // public String getFirstName() {
+    //     return firstName;
+    // }
+
+    // public void setFirstName(String firstName) {
+    //     this.firstName = firstName;
+    // }
+
+    // public String getLastName() {
+    //     return lastName;
+    // }
+
+    // public void setLastName(String lastName) {
+    //     this.lastName = lastName;
+    // }
+
+    // public String getEmail() {
+    //     return email;
+    // }
+
+    // public void setEmail(String email) {
+    //     this.email = email;
+    // }
+
+    // public String getPhoneNumber() {
+    //     return phoneNumber;
+    // }
+
+    // public void setPhoneNumber(String phoneNumber) {
+    //     this.phoneNumber = phoneNumber;
+    // }
+
+    // public String getDepartment() {
+    //     return department;
+    // }
+
+    // public void setDepartment(String department) {
+    //     this.department = department;
+    // }
+
+    // public String getDesignation() {
+    //     return designation;
+    // }
+
+    // public void setDesignation(String designation) {
+    //     this.designation = designation;
+    // }
+
+    // public LocalDate getDateOfJoining() {
+    //     return dateOfJoining;
+    // }
+
+    // public void setDateOfJoining(LocalDate dateOfJoining) {
+    //     this.dateOfJoining = dateOfJoining;
+    // }
+
+    // public double getSalary() {
+    //     return salary;
+    // }
+
+    // public void setSalary(double salary) {
+    //     this.salary = salary;
+    // }
+
+    // public boolean isActive() {
+    //     return active;
+    // }
+
+    // public void setActive(boolean active) {
+    //     this.active = active;
+    // }
+
+
 }
