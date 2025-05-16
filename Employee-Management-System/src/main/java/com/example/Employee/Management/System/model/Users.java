af41a7be85ca537215ce0316b775e59545384346
VenuGopalReddy007 <venugopalreddy7482@gmail.com>
2025-05-16 16:08:24 +0530

    Update Users.java

--- Employee-Management-System/src/main/java/com/example/Employee/Management/System/model/Users.java
+++ Employee-Management-System/src/main/java/com/example/Employee/Management/System/model/Users.java
@@ -21,4 +21,38 @@ public class Users {
     private String role;
 
 
+    //if you are facing the problem like lombok is not working you can use the below getters and setters 
+    //     public int getId() {
+    //     return id;
+    // }
+
+    // public void setId(int id) {
+    //     this.id = id;
+    // }
+
+    // public String getUsername() {
+    //     return username;
+    // }
+
+    // public void setUsername(String username) {
+    //     this.username = username;
+    // }
+
+    // public String getPassword() {
+    //     return password;
+    // }
+
+    // public void setPassword(String password) {
+    //     this.password = password;
+    // }
+
+    // public String getRole() {
+    //     return role;
+    // }
+
+    // public void setRole(String role) {
+    //     this.role = role;
+    // }
+
+
 }
